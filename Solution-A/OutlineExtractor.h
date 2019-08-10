11b7177a8b4963f6de2d975b554dc339ce39963b
SKNJPN <sknjpn@gmail.com>
2019-08-10 09:28:49 +0900

    探索プログラムの追加

--- Solution-A/OutlineExtractor.h
+++ Solution-A/OutlineExtractor.h
@@ -2,5 +2,9 @@
 
 namespace OutlineExtractor
 {
+	bool		IsInsideImage(const Point& position, const Size& imageSize);
+	Point		GetPosition(const int direction);
+	Array<Point>	GetOutline(const Image& image, const double threshold = 140);
+	Array<Array<Point>>	GetOutlines(const Image& image, const double threshold = 140);
 };
 
